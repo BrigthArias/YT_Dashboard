e8bb80c54a9c0d72f98ee9eb28554e0fbe9c2848
BrigthArias <116394234+BrigthArias@users.noreply.github.com>
2023-08-03 19:26:52 +0200

    Update Ken_dashboard.py

--- Ken_dashboard.py
+++ Ken_dashboard.py
@@ -5,10 +5,9 @@ Created on Thu Aug  3 13:03:18 2023
 @author: bariastoribio
 """
 
-#import libs
-
-import pandas as pd
-import numpy as np
+#import relevant libraries (visualization, dashboard, data manipulation)
+import pandas as pd 
+import numpy as np 
 import plotly.graph_objects as go
 import plotly.express as px
 import streamlit as st
@@ -27,8 +26,8 @@ def style_positive(v, props=''):
     try: 
         return props if v > 0 else None
     except:
-        pass   
-
+        pass    
+    
 def audience_simple(country):
     """Show top represented countries"""
     if country == 'US':
@@ -37,18 +36,14 @@ def audience_simple(country):
         return 'India'
     else:
         return 'Other'
-#load_data
-
+    
 @st.cache
 def load_data():
-
+    """ Loads in 4 dataframes and does light feature engineering"""
     df_agg = pd.read_csv('Aggregated_Metrics_By_Video.csv').iloc[1:,:]
-    df_agg_sub = pd.read_csv('Aggregated_Metrics_By_Country_And_Subscriber_Status.csv')
-    
     df_agg.columns = ['Video','Video title','Video publish time','Comments added','Shares','Dislikes','Likes',
-                          'Subscribers lost','Subscribers gained','RPM(USD)','CPM(USD)','Average % viewed','Average view duration',
-                          'Views','Watch time (hours)','Subscribers','Your estimated revenue (USD)','Impressions','Impressions ctr(%)']
-    
+                      'Subscribers lost','Subscribers gained','RPM(USD)','CPM(USD)','Average % viewed','Average view duration',
+                      'Views','Watch time (hours)','Subscribers','Your estimated revenue (USD)','Impressions','Impressions ctr(%)']
     df_agg['Video publish time'] = pd.to_datetime(df_agg['Video publish time'])
     df_agg['Average view duration'] = df_agg['Average view duration'].apply(lambda x: datetime.strptime(x,'%H:%M:%S'))
     df_agg['Avg_duration_sec'] = df_agg['Average view duration'].apply(lambda x: x.second + x.minute*60 + x.hour*3600)
@@ -59,15 +54,14 @@ def load_data():
     df_comments = pd.read_csv('Aggregated_Metrics_By_Video.csv')
     df_time = pd.read_csv('Video_Performance_Over_Time.csv')
     df_time['Date'] = pd.to_datetime(df_time['Date'])
-    
-    return df_agg, df_agg_sub, df_comments, df_time
+    return df_agg, df_agg_sub, df_comments, df_time 
+
 
 #create dataframes from the function 
 df_agg, df_agg_sub, df_comments, df_time = load_data()
 
 #additional data engineering for aggregated data 
 df_agg_diff = df_agg.copy()
-
 metric_date_12mo = df_agg_diff['Video publish time'].max() - pd.DateOffset(months =12)
 median_agg = df_agg_diff[df_agg_diff['Video publish time'] >= metric_date_12mo].median()
 
@@ -75,6 +69,7 @@ median_agg = df_agg_diff[df_agg_diff['Video publish time'] >= metric_date_12mo].
 #Just numeric columns 
 numeric_cols = np.array((df_agg_diff.dtypes == 'float64') | (df_agg_diff.dtypes == 'int64'))
 df_agg_diff.iloc[:,numeric_cols] = (df_agg_diff.iloc[:,numeric_cols] - median_agg).div(median_agg)
+
 
 #merge daily data with publish data to get delta 
 df_time_diff = pd.merge(df_time, df_agg.loc[:,['Video','Video publish time']], left_on ='External Video ID', right_on = 'Video')
@@ -84,7 +79,6 @@ df_time_diff['days_published'] = (df_time_diff['Date'] - df_time_diff['Video pub
 date_12mo = df_agg['Video publish time'].max() - pd.DateOffset(months =12)
 df_time_diff_yr = df_time_diff[df_time_diff['Video publish time'] >= date_12mo]
 
-
 # get daily view data (first 30), median & percentiles 
 views_days = pd.pivot_table(df_time_diff_yr,index= 'days_published',values ='Views', aggfunc = [np.mean,np.median,lambda x: np.percentile(x, 80),lambda x: np.percentile(x, 20)]).reset_index()
 views_days.columns = ['days_published','mean_views','median_views','80pct_views','20pct_views']
@@ -92,16 +86,18 @@ views_days = views_days[views_days['days_published'].between(0,30)]
 views_cumulative = views_days.loc[:,['days_published','median_views','80pct_views','20pct_views']] 
 views_cumulative.loc[:,['median_views','80pct_views','20pct_views']] = views_cumulative.loc[:,['median_views','80pct_views','20pct_views']].cumsum()
 
+
 ###############################################################################
 #Start building Streamlit App
 ###############################################################################
 
 
 add_sidebar = st.sidebar.selectbox('Aggregate or Individual Video', ('Aggregate Metrics','Individual Video Analysis'))
-    
-##Total Picture
 
+
+#Show individual metrics 
 if add_sidebar == 'Aggregate Metrics':
+    st.write("Ken Jee YouTube Aggregated Data")
     
     df_agg_metrics = df_agg[['Video publish time','Views','Likes','Subscribers','Shares','Comments added','RPM(USD)','Average % viewed',
                              'Avg_duration_sec', 'Engagement_ratio','Views / sub gained']]
@@ -121,43 +117,41 @@ if add_sidebar == 'Aggregate Metrics':
             count += 1
             if count >= 5:
                 count = 0
-
-#get date information / trim to relevant data 
+    #get date information / trim to relevant data 
     df_agg_diff['Publish_date'] = df_agg_diff['Video publish time'].apply(lambda x: x.date())
     df_agg_diff_final = df_agg_diff.loc[:,['Video title','Publish_date','Views','Likes','Subscribers','Shares','Comments added','RPM(USD)','Average % viewed',
                              'Avg_duration_sec', 'Engagement_ratio','Views / sub gained']]
+    
     df_agg_numeric_lst = df_agg_diff_final.median().index.tolist()
     df_to_pct = {}
     for i in df_agg_numeric_lst:
         df_to_pct[i] = '{:.1%}'.format
     
     st.dataframe(df_agg_diff_final.style.hide().applymap(style_negative, props='color:red;').applymap(style_positive, props='color:green;').format(df_to_pct))
-
+    
 if add_sidebar == 'Individual Video Analysis':
     videos = tuple(df_agg['Video title'])
     st.write("Individual Video Performance")
     video_select = st.selectbox('Pick a Video:', videos)
     
     agg_filtered = df_agg[df_agg['Video title'] == video_select]
-    
     agg_sub_filtered = df_agg_sub[df_agg_sub['Video Title'] == video_select]
     agg_sub_filtered['Country'] = agg_sub_filtered['Country Code'].apply(audience_simple)
-    agg_sub_filtered.sort_values('Is Subscribed', inplace= True)  
-
+    agg_sub_filtered.sort_values('Is Subscribed', inplace= True)   
+    
     fig = px.bar(agg_sub_filtered, x ='Views', y='Is Subscribed', color ='Country', orientation ='h')
     #order axis 
-    st.plotly_chart(fig)    
+    st.plotly_chart(fig)
     
     agg_time_filtered = df_time_diff[df_time_diff['Video Title'] == video_select]
     first_30 = agg_time_filtered[agg_time_filtered['days_published'].between(0,30)]
     first_30 = first_30.sort_values('days_published')
-   
-   
-    fig2 = go.Figure()
     
+    
+    fig2 = go.Figure()
     fig2.add_trace(go.Scatter(x=views_cumulative['days_published'], y=views_cumulative['20pct_views'],
-                mode='lines',
-                name='20th percentile', line=dict(color='purple', dash ='dash')))
+                    mode='lines',
+                    name='20th percentile', line=dict(color='purple', dash ='dash')))
     fig2.add_trace(go.Scatter(x=views_cumulative['days_published'], y=views_cumulative['median_views'],
                         mode='lines',
                         name='50th percentile', line=dict(color='black', dash ='dash')))
@@ -171,7 +165,6 @@ if add_sidebar == 'Individual Video Analysis':
     fig2.update_layout(title='View comparison first 30 days',
                    xaxis_title='Days Since Published',
                    yaxis_title='Cumulative views')
-    
     
     st.plotly_chart(fig2)
         
